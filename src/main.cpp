13e644422b3281eae91a40552485b00daed29327
seadrahbe <174991882+seadrahbe@users.noreply.github.com>
2026-01-27 19:48:27 +0000

    added multiple bug functionality, jitter functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,39 @@
+#include <bn_core.h>
+#include <bn_backdrop.h>
+#include <bn_color.h>
+#include <bn_sprite_ptr.h>
+#include <bn_random.h>
+#include <bn_vector.h>
+#include <bn_keypad.h>
 
+#include "bn_sprite_items_ant.h"
 
 int main() {
     bn::core::init();
-    
+
+    bn::random rng = bn::random();
+
+    bn::backdrop::set_color(bn::color(31, 0, 31));
+
+    bn::vector<bn::sprite_ptr, 50> bugs = {};
+
+    // bn::sprite_ptr ant = bn::sprite_items::ant.create_sprite();
+
+    while(true) {
+
+        for(bn::sprite_ptr& ant : bugs) {  
+            bn::fixed new_x = ant.x() + rng.get_fixed(-1, 1);
+            bn::fixed new_y = ant.y() + rng.get_fixed(-1, 1);
+
+            ant.set_x(new_x);
+            ant.set_y(new_y);
+        }
+
+        if(bn::keypad::a_pressed()) {
+            bugs.push_back(bn::sprite_items::ant.create_sprite());
+        }
+
+        rng.update();
+        bn::core::update();
+    }
 }
